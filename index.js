aaa3ba0e91ac7fc47d4ba8cfc77cbbbb64d67415
Franco <franco.danielsen@gmail.com>
2022-05-18 18:37:23 -0300

    adding phase 1 and 2

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 let nav = document.querySelector("#nav");
-let cards = document.querySelectorAll('.card')
+let cards = document.querySelectorAll(".card");
+let cards2 = document.querySelectorAll(".card2");
 
-window.onscroll = function() {        
-    let position = window.pageYOffset
-    console.log(window.pageYOffset)
-    if (position > 0){
-        if(position > 300){
-            cards.forEach(card => {
-            card.classList.add('show')
-            });
-        }else{
-            cards.forEach(card => {
-                card.classList.remove('show')
-                });
-        }
-        nav.classList.add('show');        
-    }else{
-        nav.classList.remove('show');        
+window.onscroll = function () {
+  let position = window.pageYOffset;
+  console.log(window.pageYOffset);
+  if (position > 0) {
+    if (position > 300) {
+      cards.forEach((card) => {
+        card.classList.add("show");
+      });
+      } else {
+        cards.forEach((card) => {
+          card.classList.remove("show");
+        });
+      }
+      if (position > 1200) {
+          cards2.forEach(card => {
+              card.classList.add('show')
+          })
+      }else {
+        cards2.forEach(card => {
+            card.classList.remove('show')
+        })
+      }
+      nav.classList.add("show");
+    } else {
+      nav.classList.remove("show");
     }
-}
+  };
